7daedb3a07aa62728db7801c6f98f720cbc586b2
Wei Song <songwei@david916.com>
2020-08-18 15:37:41 +0800

    add stream player

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,12 +1,38 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Container from "@material-ui/core/Container";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream();
+    this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (!this.player && this.props.stream) {
+      this.player = flv.createPlayer({
+        type: "flv",
+        url: `http://localhost:8000/live/${this.props.match.params.id}.flv`,
+      });
+      this.player.attachMediaElement(this.videoRef.current);
+      this.player.load();
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.player) this.player.destroy();
   }
 
   render() {
@@ -19,10 +45,9 @@ class StreamShow extends React.Component {
     }
     return (
       <Container>
-        <div>
-          <h3>{this.props.stream.title}</h3>
-          <p>{this.props.stream.description}</p>
-        </div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
+        <h3>{this.props.stream.title}</h3>
+        <p>{this.props.stream.description}</p>
       </Container>
     );
   }
